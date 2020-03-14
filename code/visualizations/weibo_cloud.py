a952ae3681da6c050e4a76b31d2871b778b3cf31
yuw072 <52962043+yuw072@users.noreply.github.com>
2020-03-13 18:04:56 -0700

    Update weibo_cloud.py

--- code/visualizations/weibo_cloud.py
+++ code/visualizations/weibo_cloud.py
@@ -48,21 +48,21 @@ def main():
     keyword = '冠状病毒'
     if keyword=='冠状病毒':
         keyword ='COVID-19'
-    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
+    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())#read data
     words = []
     for blog in mblogs:
         words.extend(jieba.analyse.extract_tags(blog['text']))
     delete_word = ["全文", "微博", "视频", "链接"]
     for i in delete_word:
         while i in words:
-            words.remove(i)
+            words.remove(i)#clean data
     gen_img(words, '../../data/Weibo/original.png',keyword)
     
     
     keyword = '武汉'
     if keyword=='武汉':
         keyword ='wuhan'
-    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
+    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())#read data
     for i in delete_word:
         while i in words:
             words.remove(i)
@@ -71,36 +71,36 @@ def main():
         words.extend(jieba.analyse.extract_tags(blog['text']))
     for i in delete_word:
         while i in words:
-            words.remove(i)
+            words.remove(i)#clean data
     gen_img(words, '../../data/Weibo/original.png',keyword)
     
     
     keyword = '口罩'
     if keyword=='口罩':
         keyword ='face-mask'
-    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
+    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())#read data
     for i in delete_word:
         while i in words:
-            words.remove(i)
+            words.remove(i)#clean data
     words = []
     for blog in mblogs:
         words.extend(jieba.analyse.extract_tags(blog['text']))
     for i in delete_word:
         while i in words:
-            words.remove(i)
+            words.remove(i)#clean data
     gen_img(words, '../../data/Weibo/original.png',keyword)
     
     
     keyword = '肺炎'
     if keyword=='肺炎':
         keyword ='pneumonia'
-    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
+    mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())#read data
     words = []
     for blog in mblogs:
         words.extend(jieba.analyse.extract_tags(blog['text']))
     for i in delete_word:
         while i in words:
-            words.remove(i)
+            words.remove(i)#clean data
     gen_img(words, '../../data/Weibo/voriginal.png',keyword)
 
     
