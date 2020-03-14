8196318b61bb145f49c96a4dc0fbcff29c747f8f
yuw072 <52962043+yuw072@users.noreply.github.com>
2020-03-13 18:03:03 -0700

    Update weibo_cloud.py

--- code/visualizations/weibo_cloud.py
+++ code/visualizations/weibo_cloud.py
@@ -49,16 +49,16 @@ def main():
     if keyword=='冠状病毒':
         keyword ='COVID-19'
     mblogs = json.loads(open('../../data/Weibo/result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
-
     words = []
     for blog in mblogs:
         words.extend(jieba.analyse.extract_tags(blog['text']))
-
     delete_word = ["全文", "微博", "视频", "链接"]
     for i in delete_word:
         while i in words:
             words.remove(i)
     gen_img(words, '../../data/Weibo/original.png',keyword)
+    
+    
     keyword = '武汉'
     if keyword=='武汉':
         keyword ='wuhan'
@@ -69,11 +69,12 @@ def main():
     words = []
     for blog in mblogs:
         words.extend(jieba.analyse.extract_tags(blog['text']))
-
     for i in delete_word:
         while i in words:
             words.remove(i)
     gen_img(words, '../../data/Weibo/original.png',keyword)
+    
+    
     keyword = '口罩'
     if keyword=='口罩':
         keyword ='face-mask'
@@ -84,11 +85,12 @@ def main():
     words = []
     for blog in mblogs:
         words.extend(jieba.analyse.extract_tags(blog['text']))
-
     for i in delete_word:
         while i in words:
             words.remove(i)
     gen_img(words, '../../data/Weibo/original.png',keyword)
+    
+    
     keyword = '肺炎'
     if keyword=='肺炎':
         keyword ='pneumonia'
